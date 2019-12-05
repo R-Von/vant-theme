17d5c274b1eae5a8ec4532e24f982964945f74c3
R-Von <778615648@qq.com>
2019-12-05 10:27:38 +0800

    feat:init the isuue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import Vant from 'vant'
 Vue.config.productionTip = false
 
+import 'vant/lib/button/style/less'
+
+Vue.use(Vant);
 new Vue({
   router,
   render: h => h(App)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <p>efretretretert</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+// import { Button } from 'vant'
+// import 'vant/lib/button/style/less'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  // components:{
+  //   'van-button':Button
+  // }
 }
 </script>
